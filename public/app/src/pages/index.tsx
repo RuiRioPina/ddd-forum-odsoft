d87200209e4e5421c56775f424a76834bae529ac
Rui Pina <rui.pina@mindera.com>
2023-09-29 14:48:12 +0100

    Development - New Posts Listing RuiRioPina/ddd-forum-odsoft-NCF_5#9 Made changes to index.sx on the folder public/pages achieve one of the goals of this exercise: to highlight today's posts with another color.

--- public/app/src/pages/index.tsx
+++ public/app/src/pages/index.tsx
@@ -1,24 +1,27 @@
-
 import React from 'react';
 import { Layout } from '../shared/layout';
 import Header from '../shared/components/header/components/Header';
-import PostFilters, { PostFilterType } from '../modules/forum/components/posts/filters/components/PostFilters';
+import PostFilters, {
+  PostFilterType
+} from '../modules/forum/components/posts/filters/components/PostFilters';
 import { Post } from '../modules/forum/models/Post';
 import { DateUtil } from '../shared/utils/DateUtil';
 import { PostRow } from '../modules/forum/components/posts/postRow';
 import { ProfileButton } from '../modules/users/components/profileButton';
 import { UsersState } from '../modules/users/redux/states';
 //@ts-ignore
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import * as usersOperators from '../modules/users/redux/operators'
-import * as forumOperators from '../modules/forum/redux/operators'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as usersOperators from '../modules/users/redux/operators';
+import * as forumOperators from '../modules/forum/redux/operators';
 import { User } from '../modules/users/models/user';
 import withLogoutHandling from '../modules/users/hocs/withLogoutHandling';
 import { ForumState } from '../modules/forum/redux/states';
 import withVoting from '../modules/forum/hocs/withVoting';
 
-interface IndexPageProps extends usersOperators.IUserOperators, forumOperators.IForumOperations {
+interface IndexPageProps
+  extends usersOperators.IUserOperators,
+    forumOperators.IForumOperations {
   users: UsersState;
   forum: ForumState;
   location: any;
@@ -29,26 +32,24 @@ interface IndexPageState {
 }
 
 class IndexPage extends React.Component<IndexPageProps, IndexPageState> {
-  constructor (props: IndexPageProps) {
+  constructor(props: IndexPageProps) {
     super(props);
 
     this.state = {
       activeFilter: 'POPULAR'
-    }
+    };
   }
 
-  onClickJoinButton () {
+  onClickJoinButton() {}
 
-  }
-
-  setActiveFilter (filter: PostFilterType) {
+  setActiveFilter(filter: PostFilterType) {
     this.setState({
       ...this.state,
       activeFilter: filter
-    })
+    });
   }
 
-  getPosts () {
+  getPosts() {
     const activeFilter = this.state.activeFilter;
 
     if (activeFilter === 'NEW') {
@@ -58,16 +59,20 @@ class IndexPage extends React.Component<IndexPageProps, IndexPageState> {
     }
   }
 
-  onFilterChanged (prevState: IndexPageState) {
+  onFilterChanged(prevState: IndexPageState) {
     const currentState: IndexPageState = this.state;
     if (prevState.activeFilter !== currentState.activeFilter) {
       this.getPosts();
     }
   }
 
-  setActiveFilterOnLoad () {
-    const showNewFilter = (this.props.location.search as string).includes('show=new');
-    const showPopularFilter = (this.props.location.search as string).includes('show=popular');
+  setActiveFilterOnLoad() {
+    const showNewFilter = (this.props.location.search as string).includes(
+      'show=new'
+    );
+    const showPopularFilter = (this.props.location.search as string).includes(
+      'show=popular'
+    );
 
     let activeFilter = this.state.activeFilter;
 
@@ -78,10 +83,10 @@ class IndexPage extends React.Component<IndexPageProps, IndexPageState> {
     this.setState({
       ...this.state,
       activeFilter
-    })
+    });
   }
 
-  getPostsFromActiveFilterGroup (): Post[] {
+  getPostsFromActiveFilterGroup(): Post[] {
     if (this.state.activeFilter === 'NEW') {
       return this.props.forum.recentPosts;
     } else {
@@ -89,17 +94,16 @@ class IndexPage extends React.Component<IndexPageProps, IndexPageState> {
     }
   }
 
-  componentDidUpdate (prevProps: IndexPageProps, prevState: IndexPageState) {
-    this.onFilterChanged(prevState)
+  componentDidUpdate(prevProps: IndexPageProps, prevState: IndexPageState) {
+    this.onFilterChanged(prevState);
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.setActiveFilterOnLoad();
     this.getPosts();
   }
 
-  render () {
-    console.log(this.props)
+  render() {
     const { activeFilter } = this.state;
 
     return (
@@ -111,34 +115,58 @@ class IndexPage extends React.Component<IndexPageProps, IndexPageState> {
           />
           <ProfileButton
             isLoggedIn={this.props.users.isAuthenticated}
-            username={this.props.users.isAuthenticated ? (this.props.users.user as User).username : ''}
+            username={
+              this.props.users.isAuthenticated
+                ? (this.props.users.user as User).username
+                : ''
+            }
             onLogout={() => this.props.logout()}
           />
         </div>
-        <br/>
-        <br/>
-
+        <br />
+        <br />
         <PostFilters
           activeFilter={activeFilter}
           onClick={(filter) => this.setActiveFilter(filter)}
         />
+        {this.getPostsFromActiveFilterGroup().map((p, i) => {
+          /**
+           * Made changes here to achieve the goal of this exercise: to highlight today's posts with another color.
+           * Used DateUtil class to check where the date in the post's createdAt property is today.
+           *
+           * Results:
+           * If it is change the background color of the post to red.
+           * If it isn't leave the background color as it is.
+           */
 
-        {this.getPostsFromActiveFilterGroup().map((p, i) => (
-          <PostRow
-            key={i}
-            onUpvoteClicked={() => this.props.upvotePost(p.slug)}
-            onDownvoteClicked={() => this.props.downvotePost(p.slug)}
-            isLoggedIn={this.props.users.isAuthenticated}
-            {...p}
-          />
-        ))}
+          const isToday = DateUtil.isToday(new Date(p.createdAt)); // Use DateUtil.isToday to check if the post is from today
 
+          // Define a style object with red background for today's posts
+          const postStyle = isToday ? { backgroundColor: 'red' } : {};
+
+          return (
+            <div key={i} style={postStyle}>
+              <PostRow
+                onUpvoteClicked={() => this.props.upvotePost(p.slug)}
+                onDownvoteClicked={() => this.props.downvotePost(p.slug)}
+                isLoggedIn={this.props.users.isAuthenticated}
+                {...p}
+              />
+            </div>
+          );
+        })}
       </Layout>
-    )
+    );
   }
 }
 
-function mapStateToProps ({ users, forum }: { users: UsersState, forum: ForumState }) {
+function mapStateToProps({
+  users,
+  forum
+}: {
+  users: UsersState;
+  forum: ForumState;
+}) {
   return {
     users,
     forum
@@ -150,11 +178,12 @@ function mapActionCreatorsToProps(dispatch: any) {
     {
       ...usersOperators,
       ...forumOperators
-    }, dispatch);
+    },
+    dispatch
+  );
 }
 
-export default connect(mapStateToProps, mapActionCreatorsToProps)(
-  withLogoutHandling(
-    withVoting(IndexPage)
-  )
-);
+export default connect(
+  mapStateToProps,
+  mapActionCreatorsToProps
+)(withLogoutHandling(withVoting(IndexPage)));
--- src/modules/forum/useCases/post/createPost/CreatePost.ts
+++ src/modules/forum/useCases/post/createPost/CreatePost.ts
@@ -1,5 +1,4 @@
-
-import { UseCase } from "../../../../../shared/core/UseCase"; 
+import { UseCase } from "../../../../../shared/core/UseCase";
 import { IPostRepo } from "../../../repos/postRepo";
 import { Either, Result, left, right } from "../../../../../shared/core/Result";
 import { AppError } from "../../../../../shared/core/AppError";
@@ -12,24 +11,25 @@ import { PostTitle } from "../../../domain/postTitle";
 import { PostText } from "../../../domain/postText";
 import { PostSlug } from "../../../domain/postSlug";
 import { PostLink } from "../../../domain/postLink";
+import { sortedUniq } from "lodash";
 
 type Response = Either<
-  CreatePostErrors.MemberDoesntExistError |
-  AppError.UnexpectedError |
-  Result<any>,
+  | CreatePostErrors.MemberDoesntExistError
+  | AppError.UnexpectedError
+  | Result<any>,
   Result<void>
->
+>;
 
 export class CreatePost implements UseCase<CreatePostDTO, Promise<Response>> {
   private postRepo: IPostRepo;
   private memberRepo: IMemberRepo;
 
-  constructor (postRepo: IPostRepo, memberRepo: IMemberRepo) {
+  constructor(postRepo: IPostRepo, memberRepo: IMemberRepo) {
     this.postRepo = postRepo;
     this.memberRepo = memberRepo;
   }
-  
-  public async execute (request: CreatePostDTO): Promise<Response> {
+
+  public async execute(request: CreatePostDTO): Promise<Response> {
     let member: Member;
     let title: PostTitle;
     let text: PostText;
@@ -40,7 +40,6 @@ export class CreatePost implements UseCase<CreatePostDTO, Promise<Response>> {
     const { userId } = request;
 
     try {
-
       try {
         member = await this.memberRepo.getMemberByUserId(userId);
       } catch (err) {
@@ -53,13 +52,13 @@ export class CreatePost implements UseCase<CreatePostDTO, Promise<Response>> {
         return left(titleOrError);
       }
 
-      if (request.postType === 'text') {
+      if (request.postType === "text") {
         const textOrError = PostText.create({ value: request.text });
 
         if (textOrError.isFailure) {
           return left(textOrError);
         }
-        
+
         text = textOrError.getValue();
       } else {
         const linkOrError = PostLink.create({ url: request.link });
@@ -67,7 +66,7 @@ export class CreatePost implements UseCase<CreatePostDTO, Promise<Response>> {
         if (linkOrError.isFailure) {
           return left(linkOrError);
         }
-        
+
         link = linkOrError.getValue();
       }
 
@@ -86,8 +85,8 @@ export class CreatePost implements UseCase<CreatePostDTO, Promise<Response>> {
         type: request.postType,
         memberId: member.memberId,
         text,
-        link
-      }
+        link,
+      };
 
       const postOrError = Post.create(postProps);
 
@@ -99,8 +98,7 @@ export class CreatePost implements UseCase<CreatePostDTO, Promise<Response>> {
 
       await this.postRepo.save(post);
 
-      return right(Result.ok<void>())
-
+      return right(Result.ok<void>());
     } catch (err) {
       return left(new AppError.UnexpectedError(err));
     }
